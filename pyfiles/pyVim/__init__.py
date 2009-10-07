a45f6800680d27eea7c9336816593759436bf2ac
winterTTr <winterTTr@localhost>
2009-10-07 20:14:42 +0800

    rename pim to pv ( pyVim )

--- pyfiles/pyVim/__init__.py
+++ pyfiles/pyVim/__init__.py
@@ -1,52 +1,52 @@
-#from pimWrap import *
-#from pimFrameBase import *
-#from pimExBuffer import *
-#from pimExFrame import *
-#from pimFramework import *
+#from pvWrap import *
+#from pvFrameBase import *
+#from pvExBuffer import *
+#from pvExFrame import *
+#from pvFramework import *
 #
 #__all__ = [
-#        # pimWrap
-#        "pimBuffer",
-#        "pimWindow",
-#        "pimWinSplitter",
-#        "PIM_BUF_TYPE_READONLY",
-#        "PIM_BUF_TYPE_NORMAL",
-#        "PIM_SPLIT_TYPE_MOST_TOP",
-#        "PIM_SPLIT_TYPE_MOST_BOTTOM",
-#        "PIM_SPLIT_TYPE_MOST_RIGHT",
-#        "PIM_SPLIT_TYPE_MOST_LEFT",
-#        "PIM_SPLIT_TYPE_CUR_TOP",
-#        "PIM_SPLIT_TYPE_CUR_BOTTOM",
-#        "PIM_SPLIT_TYPE_CUR_LEFT",
-#        "PIM_SPLIT_TYPE_CUR_RIGHT",
-#        # pimFrameBase
-#        "pimFrame",
-#        "pimChildWindow",
-#        "PIM_FRAME_TYPE_TOP",
-#        "PIM_FRAME_TYPE_BOTTOM",
-#        "PIM_FRAME_TYPE_LEFT",
-#        "PIM_FRAME_TYPE_RIGHT",
-#        "PIM_FRAME_TYPE_FIXSIZE",
-#        "PIM_CHILDWIN_TYPE_TOP",
-#        "PIM_CHILDWIN_TYPE_BOTTOM",
-#        "PIM_CHILDWIN_TYPE_LEFT",
-#        "PIM_CHILDWIN_TYPE_RIGHT",
-#        # pimExBuffer
-#        "pimBufferException",
+#        # pvWrap
+#        "pvBuffer",
+#        "pvWindow",
+#        "pvWinSplitter",
+#        "pv_BUF_TYPE_READONLY",
+#        "pv_BUF_TYPE_NORMAL",
+#        "pv_SPLIT_TYPE_MOST_TOP",
+#        "pv_SPLIT_TYPE_MOST_BOTTOM",
+#        "pv_SPLIT_TYPE_MOST_RIGHT",
+#        "pv_SPLIT_TYPE_MOST_LEFT",
+#        "pv_SPLIT_TYPE_CUR_TOP",
+#        "pv_SPLIT_TYPE_CUR_BOTTOM",
+#        "pv_SPLIT_TYPE_CUR_LEFT",
+#        "pv_SPLIT_TYPE_CUR_RIGHT",
+#        # pvFrameBase
+#        "pvFrame",
+#        "pvChildWindow",
+#        "pv_FRAME_TYPE_TOP",
+#        "pv_FRAME_TYPE_BOTTOM",
+#        "pv_FRAME_TYPE_LEFT",
+#        "pv_FRAME_TYPE_RIGHT",
+#        "pv_FRAME_TYPE_FIXSIZE",
+#        "pv_CHILDWIN_TYPE_TOP",
+#        "pv_CHILDWIN_TYPE_BOTTOM",
+#        "pv_CHILDWIN_TYPE_LEFT",
+#        "pv_CHILDWIN_TYPE_RIGHT",
+#        # pvExBuffer
+#        "pvBufferException",
 #        "PTNT_BRANCH",
 #        "PTNT_LEEF",
-#        "pimTreeNode",
+#        "pvTreeNode",
 #        "PTB_ADD_FIRST",
 #        "PTB_ADD_LAST",
 #        "PTB_ADD_ALPHA",
-#        "pimTreeBuffer",
-#        "pimTabBuffer",
-#        # pimExFrame
-#        "pimTabFrame",
-#        # pimFramework
-#        "pimFramework"
+#        "pvTreeBuffer",
+#        "pvTabBuffer",
+#        # pvExFrame
+#        "pvTabFrame",
+#        # pvFramework
+#        "pvFramework"
 #    ]
 #
 #
 
-import pimWrap
+import pvWrap
