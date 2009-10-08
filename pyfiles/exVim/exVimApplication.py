c7f30e6ea6b4abcaa2d228accb93741cb64b0300
winterTTr <winterTTr@localhost>
2009-10-08 23:36:17 +0800

    test for the register

--- pyfiles/exVim/exVimApplication.py
+++ pyfiles/exVim/exVimApplication.py
@@ -4,15 +4,25 @@ from pyVim.pvExBuffer import pvListBuffer
 from pyVim.pvKeyMapManager import pvKeyMapManager
 from pyVim.pvKeyMapManager import PV_KMM_MODE_INSERT , PV_KMM_MODE_NORMAL
 
-def do_key( keyname , mode ):
-    print keyname , mode 
+#def do_key( keyname , mode ):
+#    print keyname , mode 
+
+class M:
+    @staticmethod
+    def do_key(  keyname , mode ):
+        import vim
+        vim.command('set noshowmode')
+        print  keyname , mode
 
 class Application:
     def __init__( self ):
         self.winmgr = pvWindowManager('(-,-)main | ( 30 , - )panel , ( -,10) list ')
         self.keymgr = pvKeyMapManager()
-        self.keymgr.register( '<C-J>' , PV_KMM_MODE_INSERT , do_key )
-        self.keymgr.register( '<C-K>' , PV_KMM_MODE_INSERT , do_key )
+
+        self.m = M()
+
+        self.keymgr.register( '<C-J>' , PV_KMM_MODE_INSERT , M.do_key )
+        self.keymgr.register( '<C-K>' , PV_KMM_MODE_INSERT , M.do_key )
 
     def start( self ):
         self.winmgr.makeWindows()
