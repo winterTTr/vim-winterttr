1f32d8358a1afc42bd3025aa89e32a1aa4d36641
winterTTr <winterTTr@localhost>
2009-10-09 13:24:50 +0800

    add the key map mode for normal

--- pyfiles/exVim/exVimApplication.py
+++ pyfiles/exVim/exVimApplication.py
@@ -12,21 +12,48 @@ class Application:
     def __init__( self ):
         self.panels_caption = ['complete' , 'buffer list' , 'file explorer' ]
         self.panels = [ pvListBuffer() , pvListBuffer() , pvListBuffer() ]
-        self.list_panel = pvListBuffer()
-        #self.keymgr.register( '<C-J>' , PV_KMM_MODE_INSERT , M.do_key )
-        #self.keymgr.register( '<C-K>' , PV_KMM_MODE_INSERT , M.do_key )
+        self.panel_list = pvListBuffer()
+
+        exVim_key_map_manager.register( '<C-J>' , PV_KMM_MODE_INSERT , self.moveItem_Panel )
+        exVim_key_map_manager.register( '<C-J>' , PV_KMM_MODE_NORMAL , self.moveItem_Panel )
+        exVim_key_map_manager.register( '<C-K>' , PV_KMM_MODE_INSERT , self.moveItem_Panel )
+        exVim_key_map_manager.register( '<C-K>' , PV_KMM_MODE_NORMAL , self.moveItem_Panel )
+
+        exVim_key_map_manager.register( '<M-j>' , PV_KMM_MODE_NORMAL , self.moveItem_List )
+        exVim_key_map_manager.register( '<M-j>' , PV_KMM_MODE_INSERT , self.moveItem_List )
+        exVim_key_map_manager.register( '<M-k>' , PV_KMM_MODE_NORMAL , self.moveItem_List )
+        exVim_key_map_manager.register( '<M-k>' , PV_KMM_MODE_INSERT , self.moveItem_List )
 
     def start( self ):
         exVim_window_manager.makeWindows('(-,-)main | ( 30 , - )panel , ( -,10) list')
 
         defaul_selection = 0 
-        self.list_panel.showBuffer( exVim_window_manager.getWindow('list') )
-        self.list_panel.data = self.panels_caption
-        self.list_panel.updateBuffer( selection = defaul_selection , resize = True )
+        self.panel_list.showBuffer( exVim_window_manager.getWindow('list') )
+        self.panel_list.data = self.panels_caption
+        self.panel_list.updateBuffer( selection = defaul_selection , resize = True )
 
         self.panels[defaul_selection].showBuffer( exVim_window_manager.getWindow('panel') )
         self.panels[defaul_selection].data = [ '11' , '22' , '33' ]
         self.panels[defaul_selection].updateBuffer( selection = 0 )
 
+
+    def moveItem_Panel( self , keyname , mode ):
+        buffer = self.panels[ self.panel_list.selection ]
+        if isinstance( buffer , pvListBuffer ):
+            buffer.updateSelection( 1 if keyname == "<C-J>" else -1 )
+
+
+    def moveItem_List( self , keyname , mode ):
+        self.panel_list.updateSelection( 1 if keyname == "<M-j>" else -1 )
+
+        buffer_to_show = self.panels[ self.panel_list.selection ]
+        buffer_to_show.showBuffer( exVim_window_manager.getWindow('panel') )
+        buffer_to_show.updateBuffer()
+
+
+
+        
+
+        
 
         
