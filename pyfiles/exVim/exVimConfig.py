16386c20f824aed1e30fbf84934fb2b423e06e7f
winterTTr <winterTTr@localhost>
2009-10-14 16:47:14 +0800

    add global config

---
+++ pyfiles/exVim/exVimConfig.py
@@ -0,0 +1,7 @@
+# describ the main window layout
+exVim_window_description = '( 30 , - )panel , ( -,10) list | (-,-)main '
+
+# show on the panel list 
+exVim_panel_list = [ 
+        [ ]
+        ]
