31749fdb03d2c28bfbcba35cdeca2ec02e8c31fc
winterTTr <winterTTr@localhost>
2009-11-02 18:12:00 +0800

    modify the string inside pyvim try to use unicode ( wrapped by pvString )

--- pyfiles/exVim/exVimFunctionPanel.py
+++ pyfiles/exVim/exVimFunctionPanel.py
@@ -7,6 +7,8 @@ from pyVim.pvTabPanel import pvTabPanelItem
 from pyVim.pvTreeBuffer import pvTreeBuffer , pvTreeNode , pvTreeNodeFactory
 from pyVim.pvTreeBuffer import PV_TREE_NODE_TYPE_BRANCH , PV_TREE_NODE_TYPE_LEEF
 
+from pyVim.pvUtil import pvString
+
 
 class exVimPanel_ContextComplete( pvTabPanelItem ):
     def __init__( self ):
@@ -43,6 +45,9 @@ class FENodeRoot( pvTreeNode ):
     def __unicode__( self ):
         return u''
 
+    def getName( self ):
+        return pvString()
+
 class FENodeDirectory( pvTreeNode ):
     def __init__( self , path ):
         super( FENodeDirectory , self ).__init__( PV_TREE_NODE_TYPE_BRANCH )
@@ -60,6 +65,13 @@ class FENodeDirectory( pvTreeNode ):
         name = os.path.basename( self.path )
         return self.path if name == u'' else name
 
+    def getName( self ):
+        name = os.path.basename( self.path )
+
+        ret = pvString()
+        ret.UnicodeString = self.path if name == u'' else name
+        return ret
+
 class FENodeFile( pvTreeNode ):
     def __init__( self , path ):
         super( FENodeFile , self ).__init__( PV_TREE_NODE_TYPE_LEEF )
@@ -68,15 +80,20 @@ class FENodeFile( pvTreeNode ):
     def __unicode__( self ):
         return os.path.basename( self.path)
 
+    def getName( self ):
+        name = pvString()
+        name.UnicodeString = os.path.basename( self.path)
+        return name
+
+
 
 class exVimFileExplorer_NodeFactory( pvTreeNodeFactory ):
     def generateNode( self , path ):
         if path == [] :
             return FENodeRoot()
         else:
-            full_path = os.path.join( *path )
-            import types
-            assert type( full_path ) ==  types.UnicodeType
+            uni_path = [ x.UnicodeString for x in path ]
+            full_path = os.path.join( *uni_path )
             if os.path.isdir( full_path ):
                 return FENodeDirectory( full_path )
             elif os.path.isfile( full_path ):
