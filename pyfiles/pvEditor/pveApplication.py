6f1fc3700c36052a4c46f07387d756d363c89335
winterTTr <winterTTr@localhost>
2009-11-12 13:43:09 +0800

    let the split window to init with width 1000 , prevent it be to too small

--- pyfiles/pvEditor/pveApplication.py
+++ pyfiles/pvEditor/pveApplication.py
@@ -6,7 +6,7 @@ class Application:
         self.wm = pvWindowManager()
 
     def start( self ):
-        self.wm.makeWindows('( 25 , - )panel , ( -,10) list | (-,-)main , ( - , 10 )cmd')
+        self.wm.makeWindows('( 25 , 10 ) list , ( 25 , - )panel | (-,-)main , ( - , 10 )cmd')
 
         from pvePanelLoader import PanelManager
         self.pm = PanelManager( self.wm )
